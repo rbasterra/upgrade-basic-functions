f31838b693fec4f3a15be867f3e7ab03a489f23a
Rafael Basterra Careaga <rbasterra@msn.com>
2021-12-26 20:46:41 +0100

    Iteration#8

---
+++ Iteracion8.js
@@ -0,0 +1,31 @@
+// Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma.
+
+const counterWords = [
+    'code',
+    'repeat',
+    'eat',
+    'sleep',
+    'code',
+    'enjoy',
+    'sleep',
+    'code',
+    'enjoy',
+    'upgrade',
+    'code'
+  ];
+
+  function repeatCounter(param) {
+    let count={};
+
+    for (let i = 0; i < param.length; i++){
+        
+        if (param[i] in count){
+            count[param[i]]++;
+        } else{
+            count[param[i]] = 1;
+        }
+    }
+    return count;
+  }
+
+  console.log(repeatCounter(counterWords));

8537defe0b7ebf7499337addc95569e9c29ac04c
Rafael Basterra Careaga <rbasterra@msn.com>
2021-12-26 19:57:22 +0100

    Iteration#3

---
+++ Iteracion3.js
@@ -0,0 +1,16 @@
+// Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz. 
+
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(param) {
+  let sum=0;
+
+  for (let i=0; i< param.length; i++){
+      sum+= param[i];
+  }
+
+  return sum;
+  
+}
+
+console.log (sumAll (numbers));

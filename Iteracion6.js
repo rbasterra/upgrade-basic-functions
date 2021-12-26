143156625d0e37d3bd6397d45a0e5b0388afea7c
Rafael Basterra Careaga <rbasterra@msn.com>
2021-12-26 20:23:36 +0100

    Iteration#6

---
+++ Iteracion6.js
@@ -0,0 +1,31 @@
+// Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados.
+
+const duplicates = [
+    'sushi',
+    'pizza',
+    'burger',
+    'potatoe',
+    'pasta',
+    'ice-cream',
+    'pizza',
+    'chicken',
+    'onion rings',
+    'pasta',
+    'soda'
+  ];
+
+
+  function removeDuplicates(param) {
+    
+    for (let i = 0; i< param.length; i++){
+        
+        for (let j=i; j< param.length; j++){
+            if (param[i]== param[j]){
+                param.splice(j,1);
+            }
+        }
+    }
+    return param;
+  }
+
+  console.log(removeDuplicates(duplicates));
